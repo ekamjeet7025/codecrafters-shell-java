1a9389833a714b2c5c7cab230fe47b4625c5f4e1
Ekamjeet Singh <ekamjeet7025@gmail.com>
2026-06-12 13:00:47 +0530

    Stage 2: Handle invalid commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("$ ");
+        String input = scanner.nextLine().trim();
+        System.out.println(input + ": command not found");
     }
 }
