dc10c1f4f88d4e83d07d2b5d9df41082e5f232f3
Ekamjeet Singh <ekamjeet7025@gmail.com>
2026-06-12 13:05:30 +0530

    Stage 3: Implement REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("$ ");
+
+        while(true){
+       System.out.print("$ ");
         String input = scanner.nextLine().trim();
         System.out.println(input + ": command not found");
+        }
     }
 }
