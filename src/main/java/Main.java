236a5b8b61399924bfa4d3910786c5d9a1ff795d
Ekamjeet Singh <ekamjeet7025@gmail.com>
2026-06-12 13:08:19 +0530

    Stage 4: Implement exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        while(true){
-       System.out.print("$ ");
-        String input = scanner.nextLine().trim();
-        System.out.println(input + ": command not found");
+        while (true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine().trim();
+
+            if (input.equals("exit 0")) {
+                System.exit(0);
+            }
+
+            System.out.println(input + ": command not found");
         }
     }
 }
